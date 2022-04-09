2509171bb76174eebf627b89f1cb0d5a635e372f
Don Truong <dong23102003@gmail.com>
2022-04-09 03:09:41 -0400

    Add user info

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-db.collection("a").doc("a").set(data);
-
 app.use(express.static(__dirname + '/src'));
 
 app.listen(3000, function () {
     console.log("Server is running on localhost3000");
 });
+
+module.exports = admin;
--- src/models/Info.js
+++ src/models/Info.js
@@ -1,24 +1,47 @@
-import { doc, setDoc } from "firebase/firestore"; 
+const firebaseConfig = {
+    apiKey: "AIzaSyAKx4t0ds5UsCwLeY3QZJHYYGUItVqy8uM",
+    authDomain: "spartahack-e0815.firebaseapp.com",
+    databaseURL: "https://spartahack-e0815-default-rtdb.firebaseio.com",
+    projectId: "spartahack-e0815",
+    storageBucket: "spartahack-e0815.appspot.com",
+    messagingSenderId: "227707235393",
+    appId: "1:227707235393:web:39a2e0a5d3e7194167eac0",
+    measurementId: "G-QYYWSQRTBF"
+  };
 
-class Info {
-    constructor(userId, name, address, phone, email, itemId) {
+firebase.initializeApp(firebaseConfig);
+var ref = firebase.database().ref();
+
+export class Info {
+    constructor(userId, name, address, phone, email, itemId, interest) {
         this.userId = userId;
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.email = email;
         this.itemId = itemId;
+        this.interest = interest;
     }
 
-    static async write() {
-        await setDoc(doc(this.userId), {
+    async update() {
+        await ref.child(this.userId).set({
             name: this.name,
             address: this.address,
             phone: this.phone,
             email: this.email,
-            itemId: this.itemId
+            itemId: this.itemId,
+            interest: this.interest
+        });
+    }
+
+    async get() {
+        await ref.child(this.userId).get().then((snapshot) => {
+            const dict = snapshot.val();
+            this.name = dict.name;
+            this.address = dict.address;
+            this.phone = dict.phone;
+            this.email = dict.email;
+            this.itemId = dict.itemId;
         });
     }
 }
-
-module.exports = Info;
