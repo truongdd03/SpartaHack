2d7eb1f8e9add37885b13493a62ff9581387922a
Don Truong <dong23102003@gmail.com>
2022-04-09 19:14:47 -0400

    Finish items view

--- src/views/User/Items/items.js
+++ src/views/User/Items/items.js
@@ -1,4 +1,5 @@
 var items = new Items("", "", "");
+var storage = firebase.storage().ref();
 
 function load() {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -7,6 +8,8 @@ function load() {
             items.get().then(() => {
                 $("#itemName").val(items.itemName);
                 $("#description").val(items.description);
+                $("#itemImage").attr("src", items.itemImage);
+                console.log(items.itemImage);
             });
         }
     });
@@ -18,9 +21,19 @@ function saveClick() {
             items.itemId = firebase.auth().currentUser.uid;
             items.itemName = $("#itemName").val();
             items.description = $("#description").val();
-            items.update().then(() => {
-                window.alert("Sucessfully saved!");
-            });
+            const file = document.getElementById('uploadImage').files[0];
+
+            storage.child("Images").child(items.itemId + ".jpg").put(file).then((snapshot) => {
+                var path = items.itemId + ".jpg";
+                storage.child('/Images/' + path).getDownloadURL().then(function(url) {
+                    items.itemImage = url;
+                    items.update().then(() => {
+                        window.alert("Sucessfully saved!");
+                    });
+                })
+            })
+            
+
         }
     });
 }
