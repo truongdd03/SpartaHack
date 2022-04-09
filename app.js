e996b6f0a7a2ac2afe0d91201b325b9b42192549
Don Truong <dong23102003@gmail.com>
2022-04-09 00:30:27 -0400

    Add firebase

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+var admin = require("firebase-admin");
+var serviceAccount = require("./spartahack-e0815-firebase-adminsdk-5bk5j-4e1ab3c746.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+db.collection("a").doc("a").set(data);
 
 app.use(express.static(__dirname + '/src'));
 
