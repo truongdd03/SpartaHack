70977406799f8f7987be656881244f0519c5ef61
Don Truong <dong23102003@gmail.com>
2022-04-09 17:16:12 -0400

    Finish profile information

--- src/views/UserInfo/userInfo.js
+++ src/views/UserInfo/userInfo.js
@@ -1,22 +1,31 @@
-var userInfo = new Info(0, 0, 0, 0, 0, 0, []);
+var userInfo = new Info("", "", "", "", "", "", "", "", "", "", "");
 
 function load() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             userInfo.userId = firebase.auth().currentUser.uid;
-            userInfo.get();
+            userInfo.get().then(() => {
+                $("#firstName").val(userInfo.name);
+                $("#lastName").val(userInfo.l_name);
+                $("#address").val(userInfo.address);
+                $("#apt").val(userInfo.apt_num);
+                $("#city").val(userInfo.city);
+                $("#zip").val(userInfo.zip_code);
+                $("#mail").val(userInfo.email);
+                $("#phone").val(userInfo.phone);
+            });
         }
     });
 }
 
 function saveClick() {
-    console.log(userInfo);
-    var f_name = $("#firstName").val();
-    var l_name = $("#lastName").val();
-    var addr = $("#address").val();
-    var app_num = $("#apt").val();
-    var city = $("#city").val();
-    var zip_code = $("#zip").val();
-    var mail = $("#mail").val();
-    var p_num = $("#phone").val();
+    userInfo.name = $("#firstName").val();
+    userInfo.l_name = $("#lastName").val();
+    userInfo.address = $("#address").val();
+    userInfo.apt_num = $("#apt").val();
+    userInfo.cit = $("#city").val();
+    userInfo.zip_code = $("#zip").val();
+    userInfo.email = $("#mail").val();
+    userInfo.phone = $("#phone").val();
+    userInfo.update();
 }
