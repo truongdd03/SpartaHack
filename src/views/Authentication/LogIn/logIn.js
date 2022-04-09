8ef7a747c397b1bba519b1133c4475039b95f33d
Don Truong <dong23102003@gmail.com>
2022-04-09 05:28:30 -0400

    Reformat code

--- src/views/Authentication/LogIn/logIn.js
+++ src/views/Authentication/LogIn/logIn.js
@@ -15,14 +15,14 @@ firebase.initializeApp(firebaseConfig);
 function logIn() {
     var email = $("#email").val();
     var password = $("#password").val();
-        
+
     firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-        
+
         $(".loader-wrapper").fadeOut("slow");
         window.alert("Welcome back!");
         window.location.href = "../../HomePage/homePage.html";
 
-    }).catch(function(error) {
+    }).catch(function (error) {
 
         var errorMessage = error.message;
         window.alert(errorMessage);
@@ -41,15 +41,15 @@ function logIn() {
 
 };
 
-$('.validate-form .input100').each(function(){
-    $(this).focus(function(){
+$('.validate-form .input100').each(function () {
+    $(this).focus(function () {
         hideValidate(this);
     });
 });
 
-function validate (input) {
-    if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-        if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {        
+function validate(input) {
+    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
             return false;
         }
     }
